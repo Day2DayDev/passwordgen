7c6861ee95b5a25a875e14dee8c18514ebff9727
Day2DayDev <jonathandivinaramos@gmail.com>
2023-03-08 02:15:06 -0500

    Initial Commit - major improvements

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
         async function fetchData() {
           const response = await fetch('https://passwordinator.onrender.com?num=true&char=true&caps=true&len=18');
           const data = await response.json();//If I had a hugr list of predetermined passwords, I would need to use .then and ossible .map to parse through, however since this is a randomized tool, I did not need to as there would be no array of objects.
-          const container = document.getElementById('container');
+          const container = document.getElementById('output');
             container.innerHTML = JSON.parse(JSON.stringify(data.data)); // I spent 3 hours finding out how to make the code above display the password without the word "data" from the API. The simple fix was to add ".data" as the argument. Was originally container.innerHTML = JSON.stringify(data);
             //I was able to apply this to the code, to remove the quotiations surrounding the data, which is from the API/JSON file. - const users = JSON.parse(JSON.stringify(data));
             
